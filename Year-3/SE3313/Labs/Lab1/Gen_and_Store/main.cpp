e283510afe8b0c4f53de103774bcc21a2f372e6a
vboxuser <vboxuser@SE3313.myguest.virtualbox.org>
2023-01-31 22:49:15 -0500

    started lab 1

---
+++ Year-3/SE3313/Labs/Lab1/Gen_and_Store/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string text;
+
+    std::cout << "Please Enter Some Text:";
+
+    std::cin >> text;
+
+    std::cout << text;
+    
+    return 0;
+}
+
